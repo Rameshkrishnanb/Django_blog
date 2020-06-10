6f83333e97037bfe2ae59ccbed4c07c552fcb732
Rameshkrishnanb <rameshbkrishnan75@gmail.com>
2020-06-10 18:34:19 +0530

    deployfiles

--- django_project/settings.py
+++ django_project/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -131,9 +132,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
-
-
-
-
-
-
